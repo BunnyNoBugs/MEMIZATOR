1ab0413bd1dc4864654b65771ed289737e459af9
NoBugsHere <eugene200020@gmail.com>
2019-02-01 02:58:00 +0300

    don'teat_inator added

--- MEMIZATOR.py
+++ MEMIZATOR.py
@@ -1,10 +1,9 @@
-def suck_inator(text):
+def donteat_inator(text):
     for i, word in enumerate(text):
         if word.rstrip(".,\"'!?:;1234567890—()«»").endswith("шь"):
-            splitted_word = word.partition("ется")
-            text[i] = "(" + splitted_word[0] + "еться" + ")" + splitted_word[2]
-            text[i-1] = text[i-1] + ":"
-    text = " ".join(text)
+            splitted_word = word.partition(word.rstrip(".,\"'!?:;1234567890—()«»"))
+            text[i] = splitted_word[1][:-1] + splitted_word[2]
+    return text
 
 
 def mouse_inator(text):
@@ -14,14 +13,16 @@ def mouse_inator(text):
                 splitted_word = word.partition("ется")
                 text[i] = "(" + splitted_word[0] + "еться" + ")" + splitted_word[2]
                 text[i-1] = text[i-1] + ":"
-    text = " ".join(text)
     return text
 
 
 def main():
-    text = input("Введите, пожалуйста, текст для мышезации: ")
+    text = input("Введите, пожалуйста, текст для мемизации: ")
     text = text.split()
-    print(mouse_inator(text))
+    text = donteat_inator(text)
+    text = mouse_inator(text)
+    text = " ".join(text)
+    print(text)
 
 
 if __name__ == '__main__':
